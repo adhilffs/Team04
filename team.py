587cab14ca1147fccea0ea13911134a0d623adfc
adhilffs <adhilahamed492@gmail.com>
2022-04-19 14:31:56 +0530

    change3

--- team.py
+++ team.py
@@ -1,47 +1,25 @@
+# import the opencv library
 import cv2
-from imutils.video.pivideostream import PiVideoStream
-import imutils
-import time
-import numpy as np
 
-class VideoCamera(object):
-    def __init__(self, flip = False):
-        self.vs = PiVideoStream().start()
-        self.flip = flip
-        time.sleep(2.0)
+# define a video capture object
+vid = cv2.VideoCapture(0)
 
-    def __del__(self):
-        self.vs.stop()
+while (True):
 
-    def flip_if_needed(self, frame):
-        if self.flip:
-            return np.flip(frame, 0)
-        return frame
+    # Capture the video frame
+    # by frame
+    ret, frame = vid.read()
 
-    def get_frame(self):
-        frame = self.flip_if_needed(self.vs.read())
-        ret, jpeg = cv2.imencode('.jpg', frame)
-        return jpeg.tobytes()
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
 
-    def get_object(self, classifier):
-        found_objects = False
-        frame = self.flip_if_needed(self.vs.read()).copy()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-        objects = classifier.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
-
-        if len(objects) > 0:
-            found_objects = True
-
-        # Draw a rectangle around the objects
-        for (x, y, w, h) in objects:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        ret, jpeg = cv2.imencode('.jpg', frame)
-        return (jpeg.tobytes(), found_objects)
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
